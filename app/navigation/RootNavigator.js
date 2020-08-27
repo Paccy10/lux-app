0b6f020cd6cf2dadc15eea47e21fd75139c6eff5
P.C Ndayisenga <pacifiqueclement@gmail.com>
2020-08-27 17:09:06 +0200

    User profile validation

--- app/navigation/RootNavigator.js
+++ app/navigation/RootNavigator.js
@@ -6,27 +6,44 @@ import { isLoaded, isEmpty } from 'react-redux-firebase';
 
 import AuthNavigator from './AuthNavigator';
 import MainNavigator from './MainNavigator';
+import SetupScreen from '../screens/Auth/Setup';
+import routes from './routes';
+import defaultOptions from './defaultOptions';
 
 const Stack = createStackNavigator();
 
-const rootNavigator = ({ auth }) => {
+const rootNavigator = ({ auth, profile }) => {
   if (!isLoaded(auth)) {
     return <AppLoading />;
   }
 
+  let route = null;
+  if (!isEmpty(auth)) {
+    if (profile.isEmpty) {
+      route = (
+        <Stack.Screen
+          name={routes.SETUP}
+          component={SetupScreen}
+          options={{ headerShown: true }}
+        />
+      );
+    } else {
+      route = <Stack.Screen name='Main' component={MainNavigator} />;
+    }
+  } else {
+    route = <Stack.Screen name='Auth' component={AuthNavigator} />;
+  }
+
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      {!isEmpty(auth) ? (
-        <Stack.Screen name='Main' component={MainNavigator} />
-      ) : (
-        <Stack.Screen name='Auth' component={AuthNavigator} />
-      )}
+    <Stack.Navigator screenOptions={{ ...defaultOptions, headerShown: false }}>
+      {route}
     </Stack.Navigator>
   );
 };
 
 const mapStateToProps = (state) => ({
   auth: state.firebase.auth,
+  profile: state.firebase.profile,
 });
 
 export default connect(mapStateToProps)(rootNavigator);
--- app/navigation/routes.js
+++ app/navigation/routes.js
@@ -2,4 +2,5 @@ export default Object.freeze({
   WELCOME: 'Welcome',
   REGISTER: 'Register',
   LOGIN: 'Login',
+  SETUP: 'Setup',
 });
